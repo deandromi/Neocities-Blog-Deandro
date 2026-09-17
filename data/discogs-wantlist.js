bff9d32a122d0f27a0a613f1087045db4eee46b6
deandromi <deandromi@icloud.com>
2026-09-17 23:24:45 +0200

    Add files via upload

---
+++ data/discogs-wantlist.js
@@ -0,0 +1,9 @@
+window.DEANDRO_DISCOGS_DATA = {
+  "status": "setup-required",
+  "generatedAt": null,
+  "username": "Deandro",
+  "profileUrl": "https://www.discogs.com/user/Deandro",
+  "wantlistUrl": "https://www.discogs.com/wantlist?user=Deandro",
+  "total": 0,
+  "releases": []
+};
